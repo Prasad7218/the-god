46677dcee6691da9889bd0beb49bb54fad8f89da
Prasad8311 <prasad.yerawar@vshsolutions.com>
2023-09-07 13:19:59 +0530

    audio added

--- src/components/middle/Middle.tsx
+++ src/components/middle/Middle.tsx
@@ -4,7 +4,93 @@ import hanuman from "../../../public/images/hanuman.jpg";
 
 const Middle = () => {
   return (
-    <div>
+    <div
+      style={{
+        margin: "10px",
+        display: "flex",
+        gap: "10px",
+        justifyContent: "space-around",
+        flexWrap: "wrap",
+      }}
+    >
+      <AvatarImg
+        alt="hanuman"
+        img={hanuman.src}
+        url="/hanumanchalisa"
+        imgText="हनुमान चालीसा"
+      />
+      <AvatarImg
+        alt="hanuman"
+        img={hanuman.src}
+        url="/hanumanchalisa"
+        imgText="हनुमान चालीसा"
+      />
+      <AvatarImg
+        alt="hanuman"
+        img={hanuman.src}
+        url="/hanumanchalisa"
+        imgText="हनुमान चालीसा"
+      />
+      <AvatarImg
+        alt="hanuman"
+        img={hanuman.src}
+        url="/hanumanchalisa"
+        imgText="हनुमान चालीसा"
+      />
+      <AvatarImg
+        alt="hanuman"
+        img={hanuman.src}
+        url="/hanumanchalisa"
+        imgText="हनुमान चालीसा"
+      />
+      <AvatarImg
+        alt="hanuman"
+        img={hanuman.src}
+        url="/hanumanchalisa"
+        imgText="हनुमान चालीसा"
+      />
+      <AvatarImg
+        alt="hanuman"
+        img={hanuman.src}
+        url="/hanumanchalisa"
+        imgText="हनुमान चालीसा"
+      />
+      <AvatarImg
+        alt="hanuman"
+        img={hanuman.src}
+        url="/hanumanchalisa"
+        imgText="हनुमान चालीसा"
+      />
+      <AvatarImg
+        alt="hanuman"
+        img={hanuman.src}
+        url="/hanumanchalisa"
+        imgText="हनुमान चालीसा"
+      />
+      <AvatarImg
+        alt="hanuman"
+        img={hanuman.src}
+        url="/hanumanchalisa"
+        imgText="हनुमान चालीसा"
+      />
+      <AvatarImg
+        alt="hanuman"
+        img={hanuman.src}
+        url="/hanumanchalisa"
+        imgText="हनुमान चालीसा"
+      />
+      <AvatarImg
+        alt="hanuman"
+        img={hanuman.src}
+        url="/hanumanchalisa"
+        imgText="हनुमान चालीसा"
+      />
+      <AvatarImg
+        alt="hanuman"
+        img={hanuman.src}
+        url="/hanumanchalisa"
+        imgText="हनुमान चालीसा"
+      />
       <AvatarImg
         alt="hanuman"
         img={hanuman.src}
--- src/pages/hanumanchalisa.tsx
+++ src/pages/hanumanchalisa.tsx
@@ -2,16 +2,26 @@ import ImgCard from "@/components/imagecard/ImgCard";
 import Text from "@/components/text/Text";
 import React from "react";
 import hanumanImg from "../../public/images/hanuman.jpg";
-
+import Button from "@mui/material/Button";
+import { useRouter } from "next/router";
 const hanumanchalisa = () => {
+  const router = useRouter();
+  const redirectToSpecificURL = () => {
+    router.push("/");
+  };
   return (
     <div>
-      <ImgCard
-        imgAudio="/audio/hanumanchalisa.mp3"
-        title="हनुमान चालीसा"
-        hanuman={hanumanImg.src}
-      />
-      <Text />
+      <Button onClick={redirectToSpecificURL} variant="contained">
+        Back
+      </Button>
+      <div>
+        <ImgCard
+          imgAudio="/audio/hanumanchalisa.mp3"
+          title="हनुमान चालीसा"
+          hanuman={hanumanImg.src}
+        />
+        <Text />
+      </div>
     </div>
   );
 };
