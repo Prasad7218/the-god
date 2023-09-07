ca3b799ce27c7dcf86dd2534edf849743a0488ba
Prasad8311 <prasad.yerawar@vshsolutions.com>
2023-09-07 12:07:59 +0530

    hanuman chalisa added

---
+++ src/pages/hanumanchalisa.tsx
@@ -0,0 +1,12 @@
+import Text from "@/components/text/Text";
+import React from "react";
+
+const hanumanchalisa = () => {
+  return (
+    <div>
+      <Text />
+    </div>
+  );
+};
+
+export default hanumanchalisa;
---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import Header from "@/components/header/Header";
+import ImgCard from "@/components/imagecard/ImgCard";
+import Text from "@/components/text/Text";
+import Avatar from "@mui/material/Avatar";
+
+const Index = () => {
+  return (
+    <>
+      <Header />
+      <ImgCard />
+    </>
+  );
+};
+
+export default Index;
